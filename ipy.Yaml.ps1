65b1d8229b5e1491267388aebd9d30ea9125cf48
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 16:52:27 -0700

    feat(module): implement Add-IpyYaml function body

--- ipy.Yaml.ps1
+++ ipy.Yaml.ps1
@@ -5,7 +5,12 @@ function Add-IpyYaml {
         [Parameter(Mandatory)] $Engine,
         [string] $ZipPath = "$PSScriptRoot/dist/ruamel.yaml.zip"
     )
-    # WIP
+    if (-not (Test-Path $ZipPath)) {
+        throw "ruamel.yaml zip not found at '$ZipPath'. Run build.ps1 first."
+    }
+    $zipBytes = [System.IO.File]::ReadAllBytes((Resolve-Path $ZipPath))
+    $Engine.Add('/ipy/lib/site-packages', $zipBytes)
+    return $Engine
 }
 Export-ModuleMember -Function Add-IpyYaml
 })
