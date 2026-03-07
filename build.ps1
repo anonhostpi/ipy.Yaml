cc6579cca251399eacb541c85d625f0e3cf635e8
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 16:50:53 -0700

    feat(build): add build.ps1 skeleton with param block and phase regions

---
+++ build.ps1
@@ -0,0 +1,14 @@
+#Requires -Version 7.0
+param(
+    [string] $OutDir = "$PSScriptRoot/dist",
+    [string] $WorkDir = "$PSScriptRoot/.build"
+)
+#region PHASE 1: Download and extract ruamel.yaml 0.15.100
+# WIP
+#endregion
+#region PHASE 2: Apply IronPython patches
+# WIP
+#endregion
+#region PHASE 3: Zip into dist/ruamel.yaml.zip
+# WIP
+#endregion
