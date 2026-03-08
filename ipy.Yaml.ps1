bdfac1f37b06bfdcd2626349359c173a0099e8f5
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 17:25:47 -0700

    feat(module): embed namespace shim here-string for ruamel/__init__.py

--- ipy.Yaml.ps1
+++ ipy.Yaml.ps1
@@ -1,7 +1,11 @@
 #Requires -Version 7.0
 # ipy.Yaml.ps1 -- loads ruamel.yaml 0.16.13 from PyPI into an IronPythonEmbedded engine
 $WheelUrl = 'https://files.pythonhosted.org/packages/ed/c3/4c823dac2949a6baf36a4987d04c50d30184147393ba6f4bfb4c67d15a13/ruamel.yaml-0.16.13-py2.py3-none-any.whl'
-$namespaceShim = '' # WIP
+$namespaceShim = @'
+# Namespace package shim for IronPython in-memory imports.
+# The IronPythonEmbedded meta_path importer resolves ruamel.yaml directly;
+# pkgutil.extend_path is not needed and would fail in a virtual filesystem.
+'@
 $patchedInit = '' # WIP
 $patchedCompat = '' # WIP
 function Add-IpyYaml {
