7f6a59aa3b1eaed2ce7cb5848a70c774f4e70a9b
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 17:25:18 -0700

    chore(vendor): delete vendor/ directory; all files load from PyPI wheel at runtime

--- vendor/ruamel/__init__.py
+++
@@ -1,3 +0,0 @@
-# Namespace package shim for IronPython in-memory imports.
-# The IronPythonEmbedded meta_path importer resolves ruamel.yaml directly;
-# pkgutil.extend_path is not needed and would fail in a virtual filesystem.
--- vendor/ruamel/yaml/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-# Vendored ruamel.yaml 0.15.100 - patched for IronPython 3.4.2
-# Removed: _package_data (metadata only), cyaml import (C extension, unused in rt mode)
-
-version_info = (0, 15, 100)
-__version__ = '0.15.100'
-__with_libyaml__ = False
-
-from ruamel.yaml.main import *  # NOQA
