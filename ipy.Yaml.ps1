76484fec34d14b74632ee4d4ea1dc0ebc182d844
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 18:09:15 -0700

    style: remove #Requires directive

--- ipy.Yaml.ps1
+++ ipy.Yaml.ps1
@@ -1,4 +1,3 @@
-#Requires -Version 7.0
 # ipy.Yaml.ps1 -- loads ruamel.yaml 0.16.13 from PyPI into an IronPythonEmbedded engine
 $wheel_url = 'https://files.pythonhosted.org/packages/ed/c3/4c823dac2949a6baf36a4987d04c50d30184147393ba6f4bfb4c67d15a13/ruamel.yaml-0.16.13-py2.py3-none-any.whl'
 $namespace_shim = @'
