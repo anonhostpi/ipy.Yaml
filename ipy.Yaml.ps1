ec3b8e453b1070e167e7d7c24b6e2195f5a7f0fd
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 17:25:27 -0700

    refactor(module): strip old zip-based implementation from ipy.Yaml.ps1

--- ipy.Yaml.ps1
+++ ipy.Yaml.ps1
@@ -1,16 +1,6 @@
 #Requires -Version 7.0
-return (nmo {
+# ipy.Yaml.ps1 -- loads ruamel.yaml 0.16.13 from PyPI into an IronPythonEmbedded engine
 function Add-IpyYaml {
-    param(
-        [Parameter(Mandatory)] $Engine,
-        [string] $ZipPath = "$PSScriptRoot/dist/ruamel.yaml.zip"
-    )
-    if (-not (Test-Path $ZipPath)) {
-        throw "ruamel.yaml zip not found at '$ZipPath'. Run build.ps1 first."
-    }
-    $zipBytes = [System.IO.File]::ReadAllBytes((Resolve-Path $ZipPath))
-    $Engine.Add('/ipy/lib/site-packages', $zipBytes)
-    return $Engine
+    param([Parameter(Mandatory)] $Engine)
+    # WIP
 }
-Export-ModuleMember -Function Add-IpyYaml
-})
