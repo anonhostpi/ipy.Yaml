e41645a47a4be1ec7f8db3bfc613ab610922def8
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 17:59:19 -0700

    style: rename $WheelUrl to $wheel_url

--- ipy.Yaml.ps1
+++ ipy.Yaml.ps1
@@ -1,6 +1,6 @@
 #Requires -Version 7.0
 # ipy.Yaml.ps1 -- loads ruamel.yaml 0.16.13 from PyPI into an IronPythonEmbedded engine
-$WheelUrl = 'https://files.pythonhosted.org/packages/ed/c3/4c823dac2949a6baf36a4987d04c50d30184147393ba6f4bfb4c67d15a13/ruamel.yaml-0.16.13-py2.py3-none-any.whl'
+$wheel_url = 'https://files.pythonhosted.org/packages/ed/c3/4c823dac2949a6baf36a4987d04c50d30184147393ba6f4bfb4c67d15a13/ruamel.yaml-0.16.13-py2.py3-none-any.whl'
 $namespaceShim = @'
 # Namespace package shim for IronPython in-memory imports.
 # The IronPythonEmbedded meta_path importer resolves ruamel.yaml directly;
